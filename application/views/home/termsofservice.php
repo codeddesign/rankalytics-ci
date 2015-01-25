673d780529c2b3fdf8342fbf040b8c193b1dc3e5
codeddesign <web@codeddesign.org>
2015-01-25 02:52:44 -0500

    Creation of Terms page

--- application/views/home/termsofservice.php
+++ application/views/home/termsofservice.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//DE"
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 
 <head>
@@ -11,19 +11,19 @@
 <!-- end favicon -->
 
 
-<title>Datenschutz | Rankalytics Real-time SEO Marketing Analysis</title>
+<title>Terms of Service | Rankalytics Real-time SEO Marketing Analysis</title>
 <meta name="description" content="">
 
 <link rel="author" href="https://plus.google.com/u/0/107956745270665771232/">
 <a href="https://plus.google.com/107956745270665771232" rel="publisher"></a>
 
-<META NAME="Geography" CONTENT="Linde Street 2d 82216 Maisach Deutschland">
-<META NAME="Language" CONTENT="Deutsch">
+<META NAME="Geography" CONTENT="Charlotte, North Carolina">
+<META NAME="Language" CONTENT="English">
 <META NAME="Copyright" CONTENT="Copyright and Trademark by Rankalytics.com">
 <META NAME="distribution" CONTENT="Global">
-<META NAME="zipcode" CONTENT="82216">
-<META NAME="city" CONTENT="Maisach">
-<META NAME="country" CONTENT="Deutschland">
+<META NAME="zipcode" CONTENT="28278">
+<META NAME="city" CONTENT="Charlotte">
+<META NAME="country" CONTENT="United States">
 <meta name="coverage" content="Worldwide">
 <meta name="rating" content="General">
 <meta name="revisit-after" content="7 days">
@@ -67,50 +67,33 @@ clicky_site_ids.push(100773884);
 <div class="contactmap"></div>
 <span itemscope itemtype="https://schema.org/LocalBusiness">
 <div class="bodywrapper">
-	<div class="ranktracker-bottomwhitetitle">Datenschutzerklaung</div>
+	<div class="ranktracker-bottomwhitetitle">Terms of Service</div>
 	<div class="ranktracker-bottomwhitewrapper">
 		<div class="ranktracker-bottomwhitesubcontent" style="margin-bottom:70px;text-align:left;">
-		Personenbezogene Daten - Grundsätze<br>
-Rankalytics erhebt, verarbeitet und nutzt personenbezogenen Daten von Nutzern ausschließlich unter Beachtung der geltenden datenschutzrechtlichen Bestimmungen.
-Personenbezogene Daten sind alle Einzelangaben über persönliche oder sachliche Verhältnisse einer bestimmten oder bestimmbaren natürlichen Person. Hierunter fallen insbesondere sogenannte Bestandsdaten wie: Name, Anschrift, E-Mail-Adresse und Telefonnummer aber auch Nutzungsdaten können personenbezogene Daten sein, sofern sie Rückschlüsse auf eine Person zulassen – z.B. Information über den Zeitpunkt Ihres Besuchs unserer Website, den dabei von Ihnen verwendeten Browser und weitere beim Besuch unserer Seiten generierte Daten. 
-<p></p>
-Umfang der Datenerhebung, -verarbeitung und -nutzung<br>
-Wir erheben, verarbeiten und nutzen personenbezogenen Daten unserer Nutzer nur für die Zwecke der Bereitstellung dieser Website. Darüber hinaus erheben, verarbeiten und nutzen wir Ihre personenbezogenen Daten nur im Rahmen etwaiger von Ihnen gesondert erteilter Einwilligungen.
-Die Mitteilung personenbezogener Daten für den bloßen Besuch dieser Website ist nicht erforderlich. 
-Beim Zugriff auf unsere Website werden folgende Informationen protokolliert:
-Datum und Uhrzeit des Zugriffs, die URL (Internet-Adresse) der verweisenden Website, die abgerufene Datei, die übertragene Datenmenge, Meldung über den erfolgreichen Abruf, den Browsertyp und die Browserversion sowie Informationen über Ihr Betriebssystem. 
-Wir verwenden diese Informationen ausschließlich zu statistischen Zwecken sowie zu internen Analysezwecken und zur Verbesserung unseres Angebots. Rückschlüsse auf eine Person sind uns dabei nicht möglich.
-<p></p>
-Analysetools:<br>
-Google Analytics
-Rankalytics benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“). Google Analytics verwendet sog. „Cookies“, Textdateien, die auf dem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch den Nutzer ermöglichen. Die durch den Cookie erzeugten Informationen über die Benutzung von Spotwatch werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert. Aufgrund der Aktivierung der IP-Anonymisierung auf Spotwatch, wird die IP-Adresse des Nutzers von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag von Spotwatch wird Google diese Informationen benutzen, um die Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics vom Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt. Die Speicherung der Cookies kann durch eine entsprechende Einstellung der Browser-Software verhindern werden; wir weisen jedoch darauf hin, dass in diesem Fall gegebenenfalls nicht sämtliche Funktionen von Spotwatch vollumfänglich genutzt werden können. Darüber hinaus kann man die Erfassung der durch das Cookie erzeugten und auf der Nutzung der Website bezogenen Daten (inkl. der IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren wird: http://tools.google.com/dlpage/gaoptout?hl=de .
-<p></p>
-Nähere Informationen zu Nutzungsbedingungen und Datenschutz finden Sie unter http://www.google.com/analytics/terms/de.html bzw. unter http://www.google.com/intl/de/analytics/privacyoverview.html . 
-Wir weisen Sie darauf hin, dass auf dieser Webseite Google Analytics um den Code "gat._anonymizeIp();" erweitert wurde, um eine anonymisierte Erfassung von IP-Adressen (sog. IP-Masking) zu gewährleisten.
-<p></p>
-Newsletter<br>
-Sofern Sie unseren Newsletter abonniert haben, erheben und verwenden wir Ihre E-Mail-Adresse für die Zusendung unseres Newsletters. Ihre Einwilligung können Sie jederzeit mit Wirkung für die Zukunft widerrufen, so finden Sie etwa in jedem unserer Newsletter einen Abmeldelink.
-Die Bestätigung Ihrer Anmeldung erfolgt über das sogenannte Double Opt-In-Verfahren. Hierzu schicken wir Ihnen vor dem Versand des ersten Newsletters eine Bestätigungs-E-Mail mit einem Bestätigungslink. Sie erhalten unseren Newsletter erst nach Klick auf diesen Bestätigungslink.
-<p></p>
-Outsourcing<br>
-Setzt Spotwatch für bestimmte Aufgaben dritte Unternehmen im Rahmen des Outsourcings ein, so fungieren diese als Auftragsdatenverarbeiter i.S.d. § 11 BDSG. Diese wurden als solche vertraglich von Rankalytics dazu verpflichtet, die an sie zum Zwecke des Outsourcings übermittelten personenbezogenen Daten ausschließlich im Rahmen der spezifischen Aufgabe und der entsprechenden Weisungen von Rankalytics zu verarbeiten. Die Speicherung findet auf Servern in der Europäischen Union statt.
-<p></p>
-Auskunfts-, Widerspruchs- und Widerrufsrecht<br>
-Nach dem Bundesdatenschutzgesetz haben Sie das Recht auf unentgeltliche Auskunft über die zu Ihrer Person gespeicherten Daten. Zudem können Sie der Erstellung pseudonymisierter Nutzungsprofile zu Zwecken der Werbung, der Marktforschung oder der Optimierung unserer Website widersprechen. Sie haben jederzeit die Möglichkeit erteilte Einwilligungen in die Erhebung, Verarbeitung oder Nutzung Ihrer personenbezogenen Daten mit Wirkung für die Zukunft widerrufen. 
-<p></p>
-Bei eventuellen Fragen zum Thema Datenschutz und zur Verarbeitung Ihrer Daten stehen wir gerne zur Verfügung. Sie können jederzeit Auskunft über die von uns über Sie gespeicherten Daten anfordern. 
-<p></p>
-Bitte kontaktieren Sie uns unter:
-<br>
-Thomas Stehle Software Consulting<br>
-Datenschutz<br>
-Lindenstraße 2d<br>
-82216 Maisach<br>
-<p></p>
-Telefon: 0173/590 34 18<br>
-datenschutz@tomste.de
-<p></p>
-Stand 01.07.2014
+		<b>Personal data - Principles</b> <br>
+		Rankalytics may collect, process and use personal information from users exclusively in accordance with applicable data protection laws.
+		Personal data is any information concerning the personal or material circumstances of an identified or identifiable natural person. This includes primarily the so-called inventory data such as: name, address, email address and phone number also usage data can be personal data if they allow conclusions to be one person - eg Information on the date of your visit to our website, the browser you are using this and other generated data while visiting our site.
+		<p> </ p>
+		<b>Scope of data collection, processing and use</b> <br>
+		We collect, process and use personal data of our users only for the purpose of providing this website. Moreover, collect, process and use your personal data in accordance with any separately by you of consents granted.
+		The communication of personal data for the mere visit to this site is not required.
+		When you access our site the following information is logged:
+		Date and time of access, the URL (web address) of the referring site, the retrieved file, the amount of data transferred, notice of successful retrieval, browser type and browser version, and information about your operating system.
+		We use this information for statistical purposes and for internal analysis and to improve our offer. Conclusions about a person are we not possible.
+		<p> </ p>
+		<b>Analysis tools</b> <br>
+		Rankalytics uses Google Analytics, a web analytics service provided by Google Inc. ("Google"). Google Analytics uses. "Cookies", text files that are stored on your computer, to help analyze the use of the website by the user. The information generated by the cookie about your use of Rankalytics are usually transferred to a Google server in the US and stored there. Due to the activation of the IP anonymization on Rankalytics, the IP address of the user from Google will be truncated within Member States of the European Union or other parties to the Agreement on the European Economic Area. Only in exceptional cases will the full IP address transferred to a Google server in the USA and is shortened. On behalf of Rankalytics Google will use this information to evaluate your use of the website, compiling reports on website activity and providing other website activity and internet related services to the website operator. Sent by the browser as part of Google Analytics IP address is not associated with any other data held by Google. The use of cookies can be prevented by appropriate setting of the browser; We point out, however, that in this case, as appropriate, all functions can not be used to full effect of Rankalytics. In addition, you can prevent the data generated by the cookie about the use of the website (incl. The IP address) to Google and the processing of these data by Google, by downloading the available at the following link browser plugin and will install: http://tools.google.com/dlpage/gaoptout?hl=en.
+		<p> </ p>
+		For more information on Terms of Use and Privacy Policy can be found at http://www.google.com/analytics/terms.html or under http://www.google.com/analytics/privacyoverview.html.  We point out that on this website Google Analytics code "gat._anonymizeIp ();" has been extended to an anonymous collection of IP addresses (so called. IP masking).
+		<p> </ p>
+		<b>Newsletter</b> <br>
+		If you subscribe to our newsletter, we will collect and use your e-mail address to receive our newsletter. Your consent can be revoked at any time with effect for the future, so you can find about any of our newsletters an unsubscribe link.
+		The confirmation of your registration via the so-called double opt-in method. For this purpose, we will send you prior to shipment of the first newsletter a confirmation email with a confirmation link. You will receive our newsletter after you click on this confirmation link.
+		<p> </ p>
+		<b>For information, opposition and cancellation rights</b> <br>
+		According to the Federal Data Protection Act you have the right to free information about the data stored about you. In addition, you can object to the creation of pseudonymous user profiles for purposes of advertising, market research or to optimize our website. You always have the option granted consent to the collection, processing or use of your personal data with effect for the future revoked.
+		<p>
+		Updated 2015.01.25
 		</div>
 
 	</div>
